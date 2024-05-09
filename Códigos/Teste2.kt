7aa07513daa1cb65e5ad4e51c5442f1b45604c2a
Diggo 🤙🗿🤙 <125403873+RgoSL@users.noreply.github.com>
2024-05-09 14:55:37 -0300

    Teste2.kt

--- Códigos/Teste2.kt
+++ Códigos/Teste2.kt
@@ -1,1 +1,5 @@
+enumclasse de enumclasse de enum  Data de nascimento {classe Daypart  {
+   Manhã,  
 
+  Classe de dados Evento ( classe Event(
+ título de val: string val: string val título: string
